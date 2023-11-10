342c9eaa741ac1227c879fcad68dccd602a5575e
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-11-10 16:19:52 +0100

    12 fatiche wip

--- debug-excercises/le-12-fatiche-di-javascript/app.js
+++ debug-excercises/le-12-fatiche-di-javascript/app.js
@@ -1,51 +1,52 @@
-const input = document.querySelector('input');
-const array = null;
+const input = document.querySelector("input");
+const array = []; //dovrebbe essere [] invece che null
 
-input.addEventListener('keypress', function(event) {
+input.addEventListener("keypress", function (event) {
+  if (event.code != "Enter") return;
+  if (input.value.length == "") return;
 
-    if (event.code != 'Enter')    return;
-    if (input.value.length == '') return;
+  array.push(input.value); // Dobbbiamo usare push !array.add(input.value);!
 
-    array.add(input.value);
+  const li = document.createElement("li");
+  li.classList.add("list-group-item");
+  li.textContent = input.value; // dobbiamo usare textContent non text
+  document.querySelector("ul").append(li); // dobbiamo usare append non push
 
-    const li = document.createElement('li');
-    li.classList.add('list-group-item');
-    li.text = input.value;
-    document.querySelector('ul').push(li);
+  let counter = "";
+  let item = array[0];
+  const max = 1;
+  const elems = [];
 
-    let counter = '';
-    let item    = array[0];
-    const max   = 1;
-    const elems = [];
+  for (let i = 0; i < array.length; i++) {
+    //dobbiamo inserire array.length non array
+    let val = array[i];
 
-    for (let i = 0; i < array; i++) {
-        let val = array[i];
-
-        if (!elems[val]) {
-            elems[val] = 1;
-        } else {
-            elems[val]++;
-        }
-
-        for (let j = i; j < array.length; i++) {
-            if (array[i] == array[j]) {
-                counter++;
-                if (max < counter)
-                max  = counter;
-                item = array[i];
-            }
-        }
-
-        counter = 0;
+    if (!elems[val]) {
+      //dovremmo usare !elems[i]
+      elems[val] = 1;
+    } else {
+      elems[val]++;
     }
 
-    const alert = document.getElementsByClassName('alert');
+    for (let j = i; j < array.length; i++) {
+      //Dobbiamo continuare ad usare j++
+      if (array[i] == array[j]) {
+        counter++;
+        if (max < counter) max = counter; // mancano le parentesi {}
+        item = array[i]; //dovremmo usare push
+      }
+    }
 
-    alert.classList.remove('d-none');
-    alert.classList.add('d-flex');
+    counter = 0;
+  }
 
-    alert.querySelector('span:first-child').innerText = item;
-    alert.querySelector('span:last-child').innerText = max;
+  const alert = document.getElementsByClassName("alert");
 
-    console.log('${item} trovato ${max} volte');
+  alert.classList.remove("d-none");
+  alert.classList.add("d-flex");
+
+  alert.querySelector("span:first-child").innerText = item;
+  alert.querySelector("span:last-child").innerText = max;
+
+  console.log("${item} trovato ${max} volte");
 });
